26c0f6387ef3ced41f31f402504adaaccb99ec71
SmallPigPig <3552354372@qq.com>
2024-08-17 16:57:24 +0800

    修复 algolia

--- assets/AVX_problems-IjO05FnN.js
+++
@@ -1,1 +0,0 @@
-const p="/image/dpi_problems.png",s="/image/AVX_problems.png";export{p as _,s as a};
--- assets/depend-BZNAI9HR.js
+++
@@ -1,1 +0,0 @@
-const s="/image/release.png",e="/image/zip.png",p="/image/runexe.png",a="/image/depend.png";export{s as _,e as a,p as b,a as c};
